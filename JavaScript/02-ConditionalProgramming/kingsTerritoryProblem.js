47fe922b73ead5f989946c064bef43e1c0b9f803
Anushka Raj <anush6633@gmail.com>
2020-07-17 20:15:57 +0530

    Add files via upload

---
+++ JavaScript/02-ConditionalProgramming/kingsTerritoryProblem.js
@@ -0,0 +1,16 @@
+//Kings Territory Problem
+
+let king = 'John'
+
+if(true){
+    let king = 'Sam'
+
+    if(true){
+        let king = 'Ram'
+        console.log(king);
+    }
+}
+
+if(true){
+    console.log("I am second part" + king);
+}
---
+++ JavaScript/02-ConditionalProgramming/marvels.js
@@ -0,0 +1,10 @@
+//datatype - Array
+
+const superHeroes = ['Iron Man','Spider Man','Captain America','Thor']
+
+console.log(superHeroes);
+console.log(superHeroes[0]); //prints first element of the array
+
+console.log(superHeroes[superHeroes.length-1]);//prints last element of the array
+
+console.log(`We have ${superHeroes.length} super heroes`);
