4c5b8568220b39ba54fbff4429d47abc0e9b69ef
Anushka Raj <anush6633@gmail.com>
2020-07-20 20:20:55 +0530

    Add files via upload
    Objects And Array

---
+++ JavaScript/04-ObjectsAndArray/myObjects.js
@@ -0,0 +1,10 @@
+let myYoutubeVideo = {
+    title: 'Binary Calculator',
+    videoLength: 1,
+    videoCreator: 'Anushka Raj',
+    videoDescription: 'this is a video one Binary Calculator'
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`);
