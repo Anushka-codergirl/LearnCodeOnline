2b7f36dcfac1e4054782df366685f8ac53adfae8
Anushka Raj <anush6633@gmail.com>
2020-07-20 20:19:54 +0530

    Objects
    Objects

---
+++ JavaScript/Assignments/myObjects.js
@@ -0,0 +1,8 @@
+let myCourse = {
+    name: 'Javascript',
+    price: 499,
+    author: 'Hitesh Choudhary',
+    description: 'This is a javascript course for beginners'
+}
+
+console.log(`Hey there's new course ${myCourse.name} by ${myCourse.author} at price Rs.${myCourse.price} and has a description: ${myCourse.description}`);
